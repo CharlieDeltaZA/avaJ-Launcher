01452afb95fd570f0f3d446dc5a2e44eb8255035
Calvin Diogo <calvin@diogo.co.za>
2020-08-13 16:22:30 +0200

    Remove return type

--- src/avajlauncher/simulator/aircraft/Aircraft.java
+++ src/avajlauncher/simulator/aircraft/Aircraft.java
@@ -8,7 +8,7 @@ public abstract class Aircraft {
     protected Coordinates coordinates;
     private static long idCounter;    
 
-    protected void Aircraft(String name, Coordinates coordinates) {
+    protected Aircraft(String name, Coordinates coordinates) {
         // ???? 
     } 
 
