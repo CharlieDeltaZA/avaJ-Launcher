5d9274c31d2e30c95970c90fdad55ecdf23ec335
Calvin Diogo <calvin@diogo.co.za>
2020-08-13 10:32:46 +0200

    Structure and file rename

--- src/com.avaj-launcher/simulator/Simulator.java
+++ src/com.avaj-launcher/simulator/Simulator.java
@@ -1,4 +1,6 @@
-public class App {
+package simulator;
+
+public class Simulator {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
