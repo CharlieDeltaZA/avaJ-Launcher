e1804d2ece19cfb564690d6b0af6ecd08640b1d7
Calvin Diogo <calvin@diogo.co.za>
2020-08-14 11:08:40 +0200

    Reading from file

--- src/com/avajlauncher/simulator/Simulator.java
+++ src/com/avajlauncher/simulator/Simulator.java
@@ -1,13 +1,26 @@
 package com.avajlauncher.simulator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Simulator {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        String filename = args[0];
+        
         try {
-            System.out.println(args[0]);
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            System.out.println(line);
             
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Please specify a simulation file.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find simulation file '" + filename + "'");
+        } catch (IOException e) {
+            System.out.println("Error while reading file '" + filename + "'");
         }
     }
     
