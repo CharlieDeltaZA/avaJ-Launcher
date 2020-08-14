957d0fbd1621b3b496de2238888d0d391a09d77a
Calvin Diogo <calvin@diogo.co.za>
2020-08-14 12:36:09 +0200

    Basic file reading

--- src/com/avajlauncher/simulator/Simulator.java
+++ src/com/avajlauncher/simulator/Simulator.java
@@ -8,19 +8,30 @@ import java.io.IOException;
 public class Simulator {
 
     public static void main(String[] args) {
-        String filename = args[0];
         
         try {
+            String filename = args[0];
             BufferedReader reader = new BufferedReader(new FileReader(filename));
             String line = reader.readLine();
-            System.out.println(line);
+            if (line != null) {
+                System.out.println("Line one: " + line); // Debug
+                int simCount = Integer.parseInt(line); // line.split(" ")[0] ??
+                // TODO: Handle first line being NOT numbers
+                if (simCount <= 0) {
+                    System.out.println("Invalid simulation count. Must be > 0.");
+                    System.exit(1);
+                }
+
+                // Line Format
+                // TYPE NAME LAT LONG HEIGHT
+            }
             
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Please specify a simulation file.");
         } catch (FileNotFoundException e) {
-            System.out.println("Could not find simulation file '" + filename + "'");
+            System.out.println("Could not find simulation file '" + args[0] + "'");
         } catch (IOException e) {
-            System.out.println("Error while reading file '" + filename + "'");
+            System.out.println("Error while reading file '" + args[0] + "'");
         }
     }
     
