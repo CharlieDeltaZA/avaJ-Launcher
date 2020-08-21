b395cd30c352ea6e8ebc6867880fcb3a3ba3ac54
Calvin Diogo <calvin@diogo.co.za>
2020-08-21 15:01:57 +0200

    Coordinate validation before aircraft creation

--- src/com/avajlauncher/simulator/aircraft/AircraftFactory.java
+++ src/com/avajlauncher/simulator/aircraft/AircraftFactory.java
@@ -5,6 +5,18 @@ import com.avajlauncher.weather.Coordinates;
 public abstract class AircraftFactory {
     public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
 
+        // Coordinates are positive numbers
+        if (longitude <= 0 || latitude <= 0 || height <= 0) {
+            System.out.println("Coordinates must be positive numbers.");
+            System.exit(1);
+        }
+
+        // Height is in the 0-100 range
+        if (height > 100) {
+            System.out.println("Height must be between 0 and 100.");
+            System.exit(1);
+        }
+
         Coordinates coords = new Coordinates(longitude, latitude, height);
 
         switch (type) {
