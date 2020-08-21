f16f85ee3b801b3bdd441fc0f7b648e2569b6747
Calvin Diogo <calvin@diogo.co.za>
2020-08-21 13:00:19 +0200

    Remove comment

--- src/com/avajlauncher/simulator/WeatherTower.java
+++ src/com/avajlauncher/simulator/WeatherTower.java
@@ -5,7 +5,6 @@ import com.avajlauncher.weather.WeatherProvider;
 
 public class WeatherTower extends Tower {
     public String getWeather(Coordinates coordinates) {
-        // ????
         String wx = WeatherProvider.getProvider().getCurrentWeather(coordinates);
         return (wx);
     }
